76ccebbd4a02c26379533377f096ad8c097bca80
julia951 <klimuk.julia@gmail.com>
2021-09-26 13:25:14 +0300

    completed test

---
+++ src/src/test/java/demoqaRegistrationTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+
+public class demoqaRegistrationTest {
+
+    String firstName="Tester First Name";
+    String lastName="Tester First Name";
+    String email="tester@gmail.test";
+    String phoneNumber="1234567890";
+    String month="January";
+    String year="2000";
+    String subjectMath="Maths";
+    String address="testCity testStreet 123";
+    String state="Haryana";
+    String city="Panipat";
+    String successModalWindow="Thanks for submitting the form";
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void positiveFillTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").val(firstName);
+        $("#lastName").val(lastName);
+        $("#userEmail").val(email);
+        $("[name=gender][value=Other]").parent().click();
+        $("#userNumber").val(phoneNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--028:not(.react-datepicker__day--outside-month").click();
+        $("#subjectsInput").val(subjectMath).pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("./img/1.png");
+        $("#currentAddress").val(address);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
+
+        $("#submit").click();
+
+        $(".modal-title").shouldHave(text(successModalWindow));
+        $(".table-responsive").shouldHave(text(firstName+" "+lastName),text(email),text("Other"),
+                text(phoneNumber),text("28 January,2000"),text(subjectMath),text("Reading"),text("1.png"),
+                text(address),text(state +" "+ city));
+    }
+}
